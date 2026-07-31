e8b40517e7f885c53158a9768e3b0612ca1ff91b
Vadym Rusyn <v.rusyn@kitsoft.ua>
2026-07-31 15:08:18 +0300

    add login accessToken

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,10 +1,22 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { JwtModule } from '@nestjs/jwt';
 import { UsersModule } from '../users/users.module';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
 
 @Module({
-  imports: [UsersModule],
+  imports: [
+    UsersModule,
+    JwtModule.registerAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => ({
+        secret: configService.get<string>('JWT_SECRET'),
+        signOptions: { expiresIn: configService.get<string>('JWT_EXPIRES_IN') as any },
+      }),
+    }),
+  ],
   controllers: [AuthController],
   providers: [AuthService],
 })
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -4,22 +4,32 @@ import { UsersService } from '../users/users.service';
 import { Role } from '../users/role.enum';
 import { RegisterDto } from './dto/register.dto';
 import { LoginDto } from './dto/login.dto';
+import { JwtService } from '@nestjs/jwt';
 
 const SALT_ROUNDS = 10;
 
 @Injectable()
 export class AuthService {
-  constructor(private readonly usersService: UsersService) {}
+  constructor(
+    private readonly usersService: UsersService,
+    private readonly jwtService: JwtService,
+  ) {}
 
   async register(dto: RegisterDto) {
     const passwordHash = await bcrypt.hash(dto.password, SALT_ROUNDS);
     const user = await this.usersService.create(dto.fullName, dto.email, passwordHash, Role.CLIENT);
 
+    const payload = { sub: user.id, email: user.email, role: user.role };
+    const accessToken = this.jwtService.sign(payload);
+
     return {
-      id: user.id,
-      fullName: user.fullName,
-      email: user.email,
-      role: user.role,
+      accessToken,
+      user: {
+        id: user.id,
+        fullName: user.fullName,
+        email: user.email,
+        role: user.role,
+      },
     };
   }
   async login(dto: LoginDto) {
@@ -33,11 +43,18 @@ export class AuthService {
       throw new UnauthorizedException('Invalid credentials');
     }
 
+    // payload — данные, которые будут "зашиты" в токен (не секретные! видны любому, кто раскодирует base64)
+    const payload = { sub: user.id, email: user.email, role: user.role };
+    const accessToken = this.jwtService.sign(payload);
+
     return {
-      id: user.id,
-      fullName: user.fullName,
-      email: user.email,
-      role: user.role,
+      accessToken,
+      user: {
+        id: user.id,
+        fullName: user.fullName,
+        email: user.email,
+        role: user.role,
+      },
     };
   }
 }
